d22f4a0305225c37f99aa29f7f993e6048367d13
Glitch (frequency-poem) <none>
2021-07-05 07:44:55 +0000

    👚👴 Updated with Glitch

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function popupFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+function popupFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+function popupFunction3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
+function popupFunction4() {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+}
+function popupFunction5() {
+  var popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show");
+}
